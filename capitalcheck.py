bc14fbb93680db03bb1082bc8a7e9f9553bd87d4
ChirpBird <21singhamrita@gmail.com>
2020-08-18 09:30:18 -0700

    heho

--- capitalcheck.py
+++ capitalcheck.py
@@ -1,2 +1,2 @@
 x = input("Gimmme an STR!! ")
-print("It is "+str(bool(x[0].isupper()))+" that the first letter in your string is capitalized.") 
+print("It is "+str(bool(x[0].isupper()))+" that the first letter in your string is capitalized.")
