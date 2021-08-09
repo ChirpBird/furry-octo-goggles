8ea2c23e36dd413e3df847a9ed8a3d883f96b780
ChirpBird <21singhamrita@gmail.com>
2021-08-09 08:00:40 -0700

    changed numbers

--- Fractal/fractalThing.py
+++ Fractal/fractalThing.py
@@ -1,21 +1,21 @@
 import turtle
 minLen = 2
-def build_tree(t, branch_length, shorten_by, angle):
+def build_tree(t, iterations, branch_length, shorten_by, angle):
   if branch_length > minLen:
     t.forward(branch_length)
-    new_length = branch_length - shorten_by
+    new_length = branch_length/shorten_by
     t.left(angle)
 
     build_tree(t, new_length, shorten_by, angle)
-    t.right(angle*2)
+    t.right(angle)
 
     build_tree(t, new_length, shorten_by, angle)
     t.left(angle)
     t.backward(branch_length)
 
 tree = turtle.Turtle()
-#tree.hideturtle()
+tree.hideturtle()
 tree.setheading(90)
 tree.color('red')
-build_tree(tree, 50, 10, 30)
-#turtle.mainloop()
+build_tree(tree, 50, 3, 60)
+turtle.mainloop()
